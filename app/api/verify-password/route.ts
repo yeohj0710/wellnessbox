c194f2aab2b466db2baac1f119e56c9e653a99c3
yeohj0710 <yeohj0710@yonsei.ac.kr>
2025-01-09 18:08:19 +0900

    2501091808

--- app/api/verify-password/route.ts
+++ app/api/verify-password/route.ts
@@ -11,7 +11,7 @@ export async function POST(req: Request) {
       httpOnly: false,
       secure: process.env.NODE_ENV === "production",
       sameSite: "lax",
-      maxAge:  * 24,
+      maxAge: 7 * 24 * 60 * 60,
     });
     return response;
   }
