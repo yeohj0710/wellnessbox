f72d42f0e4aa0c7bb0b7d55ed9f091d9c7402d3f
yeohj0710 <yeohj0710@yonsei.ac.kr>
2025-01-31 21:53:11 +0900

    2501312153

--- components/starRating.tsx
+++ components/starRating.tsx
@@ -1,27 +1,23 @@
 import React from "react";
-import { Rating } from "react-simple-star-rating";
+// @ts-ignore
+import StarRatings from "react-star-ratings";
 
 interface StarRatingProps {
   rating: number;
   size?: number;
-  allowFraction?: boolean;
 }
 
-const StarRating: React.FC<StarRatingProps> = ({
-  rating,
-  size = 24,
-  allowFraction = true,
-}) => {
+const StarRating: React.FC<StarRatingProps> = ({ rating, size = 24 }) => {
   return (
-    <Rating
-      initialValue={rating}
-      size={size}
-      allowFraction={allowFraction}
-      readonly
-      transition
-      fillColor="#FACC15"
-      emptyColor="#D1D5DB"
-      SVGstyle={{ display: "inline-block", marginBottom: "2px" }}
+    <StarRatings
+      rating={rating}
+      starRatedColor="#FACC15"
+      starEmptyColor="#D1D5DB"
+      numberOfStars={5}
+      starDimension={`${size}px`}
+      starSpacing="2px"
+      svgIconPath="M12 .587l3.668 7.431L24 9.75l-6 5.838 1.416 8.259L12 18.99 4.584 23.847 6 15.588 0 9.75l8.332-1.732z"
+      svgIconViewBox="0 0 24 24"
     />
   );
 };
