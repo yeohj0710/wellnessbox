9dc2f5acd750c0f590db0c1d15402fccdc993ba6
yeohj0710 <yeohj0710@yonsei.ac.kr>
2025-02-21 16:27:53 +0900

    2502211627

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -62,17 +62,17 @@ export default function About() {
       </div>
       <div className="relative z-10 mt-8 w-full max-w-[800px] mx-auto px-6 sm:px-12 py-10 bg-white/0 text-center">
         <h1 className="text-3xl font-bold text-white mb-10">
-          약국에서만 파는 약국 전용 영양제, 7일치씩 주문해요
+          약국에서만 파는 약국 전용 영양제, 집에서 주문해요
         </h1>
         <p className="text-white leading-relaxed break-words">
           다른 온라인 쇼핑몰에서는 살 수 없는{" "}
           <b>약국에서만 판매하는 약국 전용 영양제</b>
           를, 웰니스박스에서 제공해요. <br />
           <br />
-          내 몸에 맞을지도 모르는 영양제, 비타민, 유산균을 한 통 단위로 사는 건
-          불필요하죠. <br />
-          약국에서만 파는 건강기능식품들을 온라인으로 편하게 확인하고, 7일분,
-          30일분씩만 복용해 보세요. <br />
+          짝퉁, 가품 논란이 끊이지 않는 다른 온라인 쇼핑몰에서 영양제, 비타민을
+          구매하는 것은 믿기 어렵죠. <br />
+          약국에서 직접 판매하는 건강기능식품들을 온라인으로 편하게 확인하고,
+          주문해 보세요. <br />
           <br />
           <b>웰니스박스</b>는 진짜인지 믿을 수 없는, 아무 영양제나 판매하는
           온라인 쇼핑몰과는 <b>3가지</b>가 다릅니다. <br />
