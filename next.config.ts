452f13841a83c20a9d661610edca91faa5b05af0
yeohj0710 <yeohj0710@yonsei.ac.kr>
2025-09-08 11:18:04 +0900

    2509081118

--- next.config.ts
+++ next.config.ts
@@ -6,6 +6,11 @@ const nextConfig: NextConfig = {
     formats: ["image/avif", "image/webp"],
     remotePatterns: [{ protocol: "https", hostname: "**" }],
   },
+
+  outputFileTracingExcludes: {
+    "*": [".next/cache/**", ".git/**", "public/**", "node_modules/.prisma/**"],
+  },
+
   webpack(config) {
     return config;
   },
